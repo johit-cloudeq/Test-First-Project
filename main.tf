2fec8cc280474f94bd0bc6c82e96dfb49f4f6220
johit-cloudeq <83412449+johit-cloudeq@users.noreply.github.com>
2021-09-15 18:22:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,89 @@
-module "resource_group" {
-    source = "git::https://github.com/cloudEQ/terraform-azurerm-rg.git"
+resource "azurerm_network_interface" "nic" {
+  name = var.nic_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  enable_accelerated_networking = var.enable_accelerated_networking
 
-    for_each = var.rg_data
-
-    resource_group_name = each.value.resource_group_name
-    resource_group_location = each.value.resource_group_location
+  ip_configuration {
+    name = var.ip_configuration_name
+    subnet_id = var.subnet_id
+    private_ip_address_allocation = var.private_ip_allocation_type
+  }
 }
 
-module "vm" {
-    source = "git::https://github.com/cloudEQ/terraform-azurerm-windowsvm.git"
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = var.vm_size
+  license_type          = var.license_type
+  availability_set_id   = var.availability_set_id
+  zone = var.zone_id == null ? null : var.zone_id
+  custom_data = var.custom_data_content == null ? null : base64encode(var.custom_data_content)
+  admin_username = var.vm_admin_name
+  admin_password = var.vm_admin_password
+  
+  boot_diagnostics {
+    storage_account_uri = var.diagnostics_storage_account_name
+  }
+  
+  os_disk {
+    name              = var.vm_name
+    caching           = var.vm_storage_disk_caching
+    storage_account_type = var.vm_storage_account_type
+  }
 
-    for_each = var.vm_data
+  // Abhishek :- Use this "source_image_id" attribute instead
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.image_version
+  }
+  
+  computer_name            = var.vm_name
+  provision_vm_agent       = var.vm_provision_agent
+  enable_automatic_updates = var.automatic_updates
 
-    nic_name = each.value.nic_name
-    location = each.value.location
-    resource_group_name = module.resource_group[each.value.rg_name].azure_resource_group_name
-    enable_accelerated_networking =each.value.enable_accelerated_networking
-    ip_configuration_name = each.value.ip_configuration_name
-    subnet_id = each.value.subnet_id
-    private_ip_allocation_type = each.value.private_ip_allocation_type
-    
-    vm_name = each.value.vm_name
-    vm_size = each.value.vm_size
-    vm_storage_disk_caching = each.value.vm_storage_disk_caching
-    vm_storage_account_type = each.value.vm_storage_account_type
-    publisher = each.value.publisher
-    offer = each.value.offer
-    sku = each.value.sku
-    image_version = each.value.image_version
-    automatic_updates = each.value.automatic_updates
-    vm_admin_name = each.value.vm_admin_name
-    vm_admin_password = each.value.vm_admin_password
-
-    datadisk = each.value.datadisk
-
-    # external_disk_name = each.value.external_disk_name
-    # external_disk_create_option = each.value.external_disk_create_option
-    # external_storage_account_type = each.value.external_storage_account_type
-    # external_disk_size = each.value.external_disk_size
-    # lun = each.value.lun
-    # external_disk_caching = each.value.external_disk_caching
-
-    nsg_name = each.value.nsg_name
+   identity {
+    type = "SystemAssigned"
+  }
 }
+
+resource "azurerm_managed_disk" "external_disks" {
+  for_each = var.datadisk
+
+  name                 = each.value.external_disk_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  create_option        = each.value.external_disk_create_option
+  storage_account_type = each.value.external_storage_account_type
+  disk_size_gb         = each.value.external_disk_size
+#   disk_encryption_set_id = "test"
+#               tags = {
+#                 environment = "staging"
+#               }
+            
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "external" {
+  for_each = var.datadisk
+
+  managed_disk_id    = azurerm_managed_disk.external_disks[each.key].id
+  virtual_machine_id = azurerm_windows_virtual_machine.vm.id
+  lun                = each.value.lun
+  caching            = each.value.external_disk_caching
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_network_interface_security_group_association" "interfaceassociation" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+© 2021 GitHub, Inc.
